05b8e12efdb1cf99a581d007894c6528373551ed
Dennis <10233439+idea404@users.noreply.github.com>
2024-02-05 16:50:12 -0500

    fix: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { type Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - TaxPal',
-    default: 'TaxPal - Accounting made simple for small businesses',
+    template: '%s - GhostAI',
+    default: 'GhostAI - Accounting made simple for small businesses',
   },
   description:
     'Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited.',
